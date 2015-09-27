45176a9e906d0ca56672a02a5ea4850538097dd1
Jacob Malachowski <jmalachowski623@gmail.com>
2015-09-26 19:25:25 -0500

    Added the functionality to post to Reddit

--- app/src/main/java/com/malachowski/quickpostforreddit/Reddit/Submission.java
+++ app/src/main/java/com/malachowski/quickpostforreddit/Reddit/Submission.java
@@ -5,9 +5,9 @@ import java.io.File;
 /**
  * Created by Jacob on 7/6/2015.
  */
-public class Submission
-{
+public class Submission {
     public String imageLink;
     public String title;
+    public String authCode;
     public String subreddit;
 }
